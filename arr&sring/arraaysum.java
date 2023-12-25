b1531037359fb21c63de2a06192dfca992e41467
krishnnavyas36 <krishnavyastr@gmail.com>
2023-12-24 20:33:28 -0800

    2

---
+++ arr&sring/arraaysum.java
@@ -0,0 +1,10 @@
+public static int arraySum(int[] numbers){
+    //Just return the sum of the list
+    //Don't print here
+    int sum = 0;
+    int i;
+    for(i = 0; i<numbers.length; i++){
+        sum = sum + numbers[i];
+        
+    }
+    return sum;
